b2f46c9c01b40ef38fe33675105895119ca228f9
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-12-22 18:08:03 +0530

    Add files via upload

--- hey.js
+++ hey.js
@@ -1,29 +1,61 @@
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 const express = require('express');
-const app = express();
-const port = 3000;
 
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+// Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// Serve the HTML form
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Define a route for the home page
+app.get('/', (_, res) => {
+    res.sendFile(__dirname + '/public/textn.html');
 });
 
+// Define a route for handling form submission
 app.post('/submit', (req, res) => {
-  const { input, filename } = req.body;
+    const text = req.body.text.replace(/Ð/g, '').replace(/\r\n|\r/g, '\n');
 
-  // Content to be sent in the response
-  const content = `${filename}:\n${input}`;
+    // Create a new PDF document
+    const doc = new PDFDocument();
 
-  // Set the response headers to trigger a file download
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Disposition', `attachment; filename=${filename}.txt`);
+    // Pipe the PDF document to a writable stream
+    const stream = fs.createWriteStream('output.pdf');
+    doc.pipe(stream);
 
-  // Send the content as the response
-  res.send(content);
+    // Add the text to the PDF document
+    doc.text(text, { encoding: 'utf8' }); // Specify the encoding as 'utf8'
+
+    // Finalize the PDF document
+    doc.end();
+
+    // Handle the completion of the PDF generation
+    stream.on('finish', () => {
+        console.log('PDF created successfully.');
+        res.download('output.pdf', 'output.pdf', (err) => {
+            if (err) {
+                console.error('Error downloading PDF:', err);
+                res.status(500).send('Error downloading PDF.');
+            } else {
+                console.log('PDF downloaded successfully.');
+            }
+        });
+    });
+
+    // Handle any errors that occur during the PDF generation
+    stream.on('error', (err) => {
+        console.error('Error creating PDF:', err);
+        res.status(500).send('Error creating PDF.');
+    });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
 });
